53a99e74ae5d8161e010234b33ea91d77926edb5
Patrick Quam <patrick@quam.computer>
2023-08-24 17:35:38 -0700

    game working

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
 import './App.css'
 import './index.css'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; // Import useEffect
 import Cell from './Cell.tsx';
 import 'tailwindcss/tailwind.css'
 
 function App() {
-
   return (
     <>
       <div className="flex justify-center items-center">
         	<h1 className='titletext'>Tic Tac Toe</h1>
     	</div>
-	<TicTacToeBoard />
+      <TicTacToeBoard />
     </>
   )
 }
 
+
 const TicTacToeBoard: React.FC = () => {
   const [board, setBoard] = useState<( 'X' | 'O' | null)[]>(Array(9).fill(null));
-  const [turn, setTurn] = useState(0); // Use state for turn
+  const [turn, setTurn] = useState(0);
+
+  const checkWinner = (board: ('X' | 'O' | null)[]) => {
+    const winningCombination = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  
+    for (const [a, b, c] of winningCombination) {
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+  
+    return null;
+  };
 
   const handleCellClick = (index: number) => {
     if (board[index]) {
-      return; // Cell already filled, do nothing
+      return;
     }
 
     const newBoard = [...board];
@@ -33,8 +54,24 @@ const TicTacToeBoard: React.FC = () => {
     }
 
     setBoard(newBoard);
-    setTurn(turn + 1);  // Increment the turn
-  }
+    setTurn(turn + 1);
+  };
+
+  // Add a useEffect to watch for board updates
+  useEffect(() => {
+    const winner = checkWinner(board);
+    if (winner) {
+      alert(`${winner} has won!`);
+      reset();
+      return;
+    }
+
+    // Check for a tie (cat's game)
+    if (turn === 8) {
+      alert("It's a tie!");
+      reset();
+    }
+  }, [board, turn]);  // The effect depends on the board and turn states
 
   return (
     <div className="justify-center items-center">
@@ -45,7 +82,10 @@ const TicTacToeBoard: React.FC = () => {
       </div>
     </div>
   );
-}
+};
 
+function reset() {
+  board.fill(null)
+};
 
 export default App
